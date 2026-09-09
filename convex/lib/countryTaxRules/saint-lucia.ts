7b986355e174892c5569677ce8f320d3b9a3cd32
joinmeetapp-a11y <joinmeetapp@gmail.com>
2026-09-09 12:13:01 -0400

    fix(country): correct shared statutory helper import

--- convex/lib/countryTaxRules/saint-lucia.ts
+++ convex/lib/countryTaxRules/saint-lucia.ts
@@ -1,5 +1,5 @@
 import { BaseTaxCalculationInput, NISCalculationResult, PAYECalculationResult, FullPayrollCalculationResult, PayFrequency } from './types';
-import { toMonthly, fromMonthly } from '../trinidad-and-tobago';
+import { toMonthly, fromMonthly } from './trinidad-and-tobago';
 
 /**
  * Saint Lucia National Insurance Corporation (NIC) & IRD PAYE Tax Rules
